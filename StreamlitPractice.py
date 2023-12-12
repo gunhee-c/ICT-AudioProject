88a42b1fb39c2c7d918f9a6bd493b4028352fc73
GunHee Cho <114210854+gunhee-c@users.noreply.github.com>
2023-12-12 23:33:18 +0900

    Update StreamlitPractice.py

--- StreamlitPractice.py
+++ StreamlitPractice.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Copied from tutorial
 # 타이틀 적용 예시
-st.title('이것은 타이틀 입니다')
+st.title('MBTI AUDIO EFFECTOR')
 
 # 특수 이모티콘 삽입 예시
 # emoji: https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
