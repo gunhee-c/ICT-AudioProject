f1483a4805571bd7d90d0e9315ff8b5a04d7f314
dnwns3437 <chorjgml@naver.com>
2023-12-13 11:38:55 +0900

    testrollback
    testrollback

--- Sandbox.py
+++ Sandbox.py
@@ -2,27 +2,29 @@ import streamlit as st
 import librosa as lr
 import numpy as np
 
-from MBTIVocalEffectPreprocess import main_preprocess
-
+#from MBTIVocalEffectPreprocess import main_preprocess
+from TryMath import st_injection
 
 
 st.header('TESTING')
 
-main_preprocess()
+#main_preprocess()
 
 tab1, tab2, tab3 = st.tabs(["메인", "Check ProgressSee Details", "MBTI 보컬 이펙터 설명"])
 
 with tab1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+    st_injection()
+
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
 
 with tab2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
 
 with tab3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
 
 
 
