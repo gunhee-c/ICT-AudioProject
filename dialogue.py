fdabe915ea38b8b65d3cedde449e3d235a6c5278
dnwns3437 <chorjgml@naver.com>
2023-12-18 01:38:38 +0900

    Update dialogue.py

--- dialogue.py
+++ dialogue.py
@@ -27,9 +27,6 @@ def tabFirst_message():
 
             st.checkbox('접기')
 
-    if (st.button('자세히 알아보기!')):
-        pass
-        #tabFirst_detail2()
 
 def tabFirst_detail():
     st.write("... 거짓말이었습니다")
