68aac3de56c70956686e5c41a2cc17ed228199b4
dnwns3437 <chorjgml@naver.com>
2023-12-18 13:37:31 +0900

    Update Sandbox.py

--- Sandbox.py
+++ Sandbox.py
@@ -162,11 +162,11 @@ with tab3:
         current_audio = audio_sample
         for i in range(index):
             if MBTIinput[i] != "None":
-                current_audio = audio_processor(current_audio, sr, ratioinput[i], MBTIinput[i], IR_audio, IR_sr)
+                processed_audio = audio_processor(current_audio, sr, ratioinput[i], MBTIinput[i], IR_audio, IR_sr)
                 st.write("Process #" + str(i+1))
                 st.write("Effect: " + MBTIinput[i] + " Ratio: " + str(ratioinput[i]))
-                audio_visualize(current_audio, sr, f"audioSampleProgress{i+1}")
-            
+                audio_visualize(processed_audio, sr, f"audioSampleProgress{i+1}")
+                processed_audio = current_audio
                 st.write("")
 
         """
