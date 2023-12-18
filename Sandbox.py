63f26f7a06e8951cc97f6969e9afba152f4b5e3f
dnwns3437 <chorjgml@naver.com>
2023-12-18 11:47:12 +0900

    Update Sandbox.py

--- Sandbox.py
+++ Sandbox.py
@@ -89,8 +89,8 @@ with tabSecond:
         end_sample = st.number_input('To which second do you want to sample?')
 
         
-        if st.button('Get your Sample!', key="button3"):
-            activate_sampler = validate_start_end(audio_length, start_sample, end_sample)
+        #if st.button('Get your Sample!', key="button3"):
+        activate_sampler = validate_start_end(audio_length, start_sample, end_sample)
         
     if activate_sampler == True:
         st.success('Your sample length is legitimate.')
